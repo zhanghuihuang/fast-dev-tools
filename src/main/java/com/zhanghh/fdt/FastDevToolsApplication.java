037eed91489b7a7309aa37cebb04c9f26e4832d4
zhanghuihuang <519241620@qq.com>
2020-04-07 18:16:17 +0800

    Initial commit

---
+++ src/main/java/com/zhanghh/fdt/FastDevToolsApplication.java
@@ -0,0 +1,13 @@
+package com.zhanghh.fdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastDevToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastDevToolsApplication.class, args);
+	}
+
+}
